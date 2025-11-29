b940aee12e1ed1d12407d9cdd8c522b8c7b1e4da
RivadonS <rivadon.sung@bumail.net>
2025-11-29 22:09:27 +0700

    player flip

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        rb.linearVelocity = new Vector2(horizontalInput * speed ,rb.linearVelocity.y);
+
+        //flip player when move left-right
+        if (horizontalInput > 0.01f)
+        {
+            transform.localScale = Vector3.one;
+        } else if (horizontalInput < -0.01f)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, speed);
+        }
+    }
+}
